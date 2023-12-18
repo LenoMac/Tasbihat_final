964f452afa7c89eeaefd64bb541b6cced4d7aa6c
bore <ibrakhimov2005@gmail.com>
2023-12-19 00:31:55 +0600

    pro

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-import { View, StatusBar, NativeModules, Button } from "react-native";
+import {
+  View,
+  StatusBar,
+  NativeModules,
+  Button,
+  SafeAreaView,
+} from "react-native";
 import { gStyle } from "./Style/Style";
 import Navigate from "./Navigate";
 import { useFonts } from "expo-font";
 import React from "react";
 
 export default function App() {
-
   // React.useEffect(() => {
-    
+
   // }, [])
   const [fontsLoaded] = useFonts({
     "Montserrat Medium": require("./assets/fonts/Montserrat Medium.ttf"),
     "Montserrat SemiBold": require("./assets/fonts/Montserrat SemiBold.ttf"),
   });
   return (
-    <View style={gStyle.app}>
-      <StatusBar backgroundColor="#320548" barStyle="light-content" />
-      <Navigate />
-    </View>
+      <View style={gStyle.app}>
+        <StatusBar backgroundColor="#320548" barStyle="light-content" />
+        <Navigate />
+      </View>
   );
 }
-
--- screens/About.js
+++ screens/About.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function About() {
   return (
-    <View>
-      
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Text style={{ alignSelf: "center" }}>Жолдошов Абдулхафиз </Text>
+      <Text style={{alignSelf: 'center'}}>Ибрахимов Байтур</Text>
     </View>
-  )
+  );
 }
