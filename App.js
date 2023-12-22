b736a4d28f5b0b2de2193de47cf927d4d6aa6858
LenoMac <hafizfullstack@gmail.com>
2023-12-23 00:30:54 +0600

    df

--- App.js
+++ App.js
@@ -11,10 +11,9 @@ import { useFonts } from "expo-font";
 import React from "react";
 
 export default function App() {
-  React.useEffect(() => {}, []);
   const [fontsLoaded] = useFonts({
     "Montserrat Medium": require("./assets/fonts/Montserrat Medium.ttf"),
-    "Montserrat SemiBold": require("./assets/fonts/Montserrat SemiBold.ttf"),
+    "Montserrat Semibold": require("./assets/fonts/Montserrat SemiBold.ttf"),
   });
   return (
     <View style={gStyle.app}>
